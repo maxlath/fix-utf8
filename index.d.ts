69393779f7a610c934c47622e58ff7279026a23f
maxlath <k@maxlath.eu>
2023-06-01 10:19:24 +0200

    add types declarations
    addressing https://github.com/maxlath/fix-utf8/issues/5

---
+++ index.d.ts
@@ -0,0 +1,7 @@
+export = fixUtf8;
+/**
+ * A function that tries its best to recover lost Latin Unicode characters
+ * @param {string} str -- text to fix
+ * @returns {string}
+ */
+declare function fixUtf8(str: string): string;
